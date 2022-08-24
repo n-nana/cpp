4cfa68f3afb07a972b035fcf7443fef2d92e0208
n_na <91656188+n-nana@users.noreply.github.com>
2022-08-24 09:37:25 +0900

    Update _template.cpp

--- _template.cpp
+++ _template.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
 int main() {
-  int n, m;
-  cin >> n >> m;
-  cout << n%m << endl;
+  int N;
+  cin >> N;
+
+//  const int K = 20;
+//  1D_Slice_int
+//  vector<int> A(K,0);
+
+  cout << N << endl;
  }
 
