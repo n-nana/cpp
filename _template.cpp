79d79ef2f136cb56c60c034eba085b3a54a62e25
n_na <91656188+n-nana@users.noreply.github.com>
2022-11-21 00:21:58 +0900

    Update _template.cpp

--- _template.cpp
+++ _template.cpp
@@ -20,12 +20,17 @@ int main() {
     vector<bool> check(N,false);
     vector<int> res(N);
     
-    vector<vector<char>> G(N, vector<char>(M, '#'));
+    vector<vector<char>> G(N, vector<char>(M, '#')); // 2次元配列（初期値は#, 初期値は指定しなくても可）
+    // 標準入力
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             cin >> G[i][j];
         }
     }
+    
+    // 3次元配列（初期値指定なし > int: 0）
+    vector<vector<vector<int>>> RU(N, vector<vector<int>>(N, vector<int>(N)));
+
     vector<vector<int>> dist(N, vector<int>(M, -1));
     queue<pair<int, int>> que;
     cout << "ok" << endl;
